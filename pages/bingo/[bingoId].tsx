c2f24e1f409019cec621c50483fdbca4113a54c9
stellarsailor <dev.stellarsailor@gmail.com>
2020-08-20 20:54:29 -0400

    deploy test2

--- pages/bingo/[bingoId].tsx
+++ pages/bingo/[bingoId].tsx
@@ -26,10 +26,10 @@ export default function list({ data }) {
             <div>
                 사이즈 : {sample.size} x {sample.size}
 
-                빙고렌더링
+                렌더링
             </div>
             신고
-            설정 - (체크 표시 모양 설정 / 배경색 / 빙고줄색 설정 - 이걸 작성자입장으로 빼야될지?)
+            설정 - (체크 표시 모양 설정 / 배경색 / 줄색 설정 - 이걸 작성자입장으로 빼야될지?)
         </Layout>
     )
 }
