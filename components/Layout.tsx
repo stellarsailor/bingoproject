9431ae662ec302108f43128ef5cbab805895191d
stellarsailor <dev.stellarsailor@gmail.com>
2020-09-21 14:19:40 -0400

    _app, _document, Header replacement

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,21 +1,23 @@
-import styled from 'styled-components'
 import Navbar from './Navbar'
-import Footer from './Footer'
-import Header from './Header'
 import { Row, Col } from 'antd'
+import Head from 'next/head'
 
 export default function Layout(props) {
 
     return(
         <div>
-            <Header />
+            <Head>
+                <title>SelfBingo</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+
+                <link rel="shortcut icon" href="/static/favicon.ico" />
+            </Head>
             <Navbar />
             <Row justify="center" style={{backgroundColor: 'var(--mono-1)', minHeight: '100vh'}}>
                 <Col xs={24} sm={22} md={20} lg={20} xl={12} >
                     {props.children}
                 </Col>
             </Row>
-            {/* <Footer /> */}
         </div>
     )
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,30 +1,65 @@
-import Document from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+// import Document from 'next/document';
+// import { ServerStyleSheet } from 'styled-components';
 
-export default class MyDocument extends Document {
+// export default class MyDocument extends Document {
+//   static async getInitialProps(ctx) {
+//     const sheet = new ServerStyleSheet();
+//     const originalRenderPage = ctx.renderPage;
+
+//     try {
+//       ctx.renderPage = () =>
+//         originalRenderPage({
+//           enhanceApp: (App) => (props) =>
+//             sheet.collectStyles(<App {...props} />),
+//         });
+
+//       const initialProps = await Document.getInitialProps(ctx);
+//       return {
+//         ...initialProps,
+//         styles: (
+//           <>
+//             {initialProps.styles}
+//             {sheet.getStyleElement()}
+//           </>
+//         ),
+//       };
+//     } finally {
+//       sheet.seal();
+//     }
+//   }
+// }
+
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
-    }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-154973389-2" />
+          <script
+            dangerouslySetInnerHTML={{
+            __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'UA-154973389-2');
+                `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
+
+export default MyDocument
