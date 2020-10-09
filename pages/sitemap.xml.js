979fcdf8607488dbf53b9c31c5ff691d97cc6100
stellarsailor <dev.stellarsailor@gmail.com>
2020-10-09 01:00:05 -0400

    sitemap test3

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { GetServerSideProps } from "next";
 import { serverUrl } from '../lib/serverUrl'
+import { appWithTranslation } from '../i18n'
 
 const createSitemap = (bingos) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -24,14 +26,27 @@ const createSitemap = (bingos) => `<?xml version="1.0" encoding="UTF-8"?>
     `;
 
 class Sitemap extends React.Component {
-    static async getInitialProps({ res }) {
-        const request = await fetch(serverUrl + '/api/bingos/sitemap');
-        const bingos = await request.json();
+//     static async getInitialProps({ res }) {
+//         const request = await fetch(serverUrl + '/api/bingos/sitemap');
+//         const bingos = await request.json();
 
-        res.setHeader('Content-Type', 'text/xml');
-        res.write(createSitemap(bingos));
-        res.end();
-    }
+//         res.setHeader('Content-Type', 'text/xml');
+//         res.write(createSitemap(bingos));
+//         res.end();
+//     }
 }
 
 export default Sitemap;
+
+export const getServerSideProps = async ({ res, req }) => {
+    const request = await fetch(serverUrl + '/api/bingos/sitemap');
+    const bingos = await request.json();
+
+    res.setHeader('Content-Type', 'text/xml');
+    res.write(createSitemap(bingos));
+    res.end();
+
+    return {
+        props: {},
+    };
+} 
