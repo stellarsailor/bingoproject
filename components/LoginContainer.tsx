2410ae54352d75dcc71f4204c0f53c148686b8ab
stellarsailor <dev.stellarsailor@gmail.com>
2020-10-12 01:53:40 -0400

    .

--- components/LoginContainer.tsx
+++ components/LoginContainer.tsx
@@ -5,14 +5,13 @@ import { serverUrl } from '../lib/serverUrl'
 
 export default function LoginContainer() {
     const router = useRouter()
-    console.log(serverUrl)
 
     return(
         <>
             <button onClick={() => signIn('google', { callbackUrl: serverUrl })}>Sign in with Google</button>
-            <button onClick={() => signIn('apple')}>Sign in with Apple</button>
-            <button onClick={() => signIn('facebook')}>Sign in with FaceBook</button>
-            <button onClick={() => signIn('twitter')}>Sign in with Twitter</button>
+            <button onClick={() => signIn('apple', { callbackUrl: serverUrl })}>Sign in with Apple</button>
+            <button onClick={() => signIn('facebook', { callbackUrl: serverUrl })}>Sign in with FaceBook</button>
+            <button onClick={() => signIn('twitter', { callbackUrl: serverUrl })}>Sign in with Twitter</button>
         </>
     )
 }
