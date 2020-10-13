3211f3afb92e96569d300961ea64adc87275887c
stellarsailor <dev.stellarsailor@gmail.com>
2020-10-12 22:59:48 -0400

    Google font added

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,13 @@
 import Navbar from './Navbar'
 import { Row, Col } from 'antd'
 import Head from 'next/head'
+import GoogleFonts from "next-google-fonts";
 
 export default function Layout(props) {
 
     return(
         <div>
+            <GoogleFonts href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" />
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
             </Head>
